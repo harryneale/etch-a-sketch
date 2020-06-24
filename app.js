6d29d54256b214722b973219cbbae5b0b35f0888
Harry Neale <harryjwneale@gmail.com>
2020-06-24 08:37:01 +0100

    Added default grid

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const grid = document.querySelector(".container");
+const gridSize = document.querySelector("#uInput");
+const go = document.querySelector(".goBtn");
+const reset = document.querySelector("#resetBtn");
 
 
+// Set default grid
+function defaultGrid() {
 
-const div = document.createElement('div');
-container.appendChild(div);
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+};
+

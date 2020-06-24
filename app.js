f60d6a7682ac17c900202c68de9d863491f7ad49
Harry Neale <harryjwneale@gmail.com>
2020-06-24 08:56:39 +0100

    Default and update grid functions added. First attempt at adding event listener to go button

--- app.js
+++ app.js
@@ -14,3 +14,33 @@ function defaultGrid() {
     }
 };
 
+// Update grid with new, user input value
+function newGrid() {
+    grid.innerHTML = '';
+    grid.style.setProperty(
+        "grid-template-columns",
+        `repeat(${gridSize.value}, 2fr)`
+    );
+    grid.style.setProperty(
+        "grid-template-rows",
+        `repeat(${gridSize.value}, 2fr)`
+    );
+    for (let i = 0; i < gridSize.value * gridSize.value ; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+    console.log(gridSize.value);
+};
+
+// Changes the class of square from square to color on mouseover
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+    event.target.classList.replace("square", "color");
+});
+
+// Add event listener to grid size input
+go.onclick = () => {
+    newGrid;
+};
+

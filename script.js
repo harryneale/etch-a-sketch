686b39ce3cbf7796359a71d7b8d8f96623273f99
Harry Neale <harryjwneale@gmail.com>
2020-06-26 09:54:20 +0100

    Linked JS file and centered grid

--- script.js
+++ script.js
@@ -36,11 +36,10 @@ function newGrid() {
 defaultGrid();
 
 // Changes the class of square from square to color on mouseover
-/* const square = document.querySelector("div");
+const square = document.querySelector("div");
 square.addEventListener("mouseover", function(event) {
     event.target.classList.replace("square", "color");
 });
 
 // Add event listener to grid size input
 gridSize.addEventListener("change", newGrid);
-

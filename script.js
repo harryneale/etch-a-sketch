cd69cc5777d75a1cdf3c2697f253be2c40459b76
Harry Neale <harryjwneale@gmail.com>
2020-06-26 16:44:26 +0100

    Reset functionality added - still want to add a shake effect

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector(".container");
-const gridSize = document.querySelector("#uInput");
+const gridSize = document.querySelector(".uInput");
+const go = document.querySelector("#goBtn");
+const clearBoard = document.querySelector("#resetBtn");
 
 
 // Set default grid
@@ -8,12 +10,45 @@ function defaultGrid() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add("square");
-        
         div.addEventListener("mouseenter", function() {
             div.style.backgroundColor = 'black';
         })
         grid.appendChild(div);
     };
 };
+
+// Set new grid size based on user input
+function newGrid() {
+    grid.innerHTML = "";
+    grid.style.setProperty(
+        "grid-template-columns",
+        `repeat(${gridSize.value}, 2fr)`
+    );
+    grid.style.setProperty(
+        "grid-template-rows",
+        `repeat(${gridSize.value}, 2fr)`
+    );
+    for (let i = 0; i < gridSize.value * gridSize.value; i++) {
+        const div = document.createElement('div');
+        div.classList.add("square");
+
+        div.addEventListener("mouseenter", function() {
+            div.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div);
+    }
+};
+
+// Add event listener to user input button
+go.addEventListener("click", newGrid);
+
+// Add reset function to clear board and reset default grid size
+clearBoard.addEventListener("click", function() {
+    grid.innerHTML = '';
+    gridSize.value = '';
+    grid.style.setProperty("grid-template-columns",`repeat(16, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+    defaultGrid();
+});
 
 defaultGrid();

a6dd0bc76d0b29ec63e5cb8d3efd3e64daefa1d8
Harry Neale <harryjwneale@gmail.com>
2020-06-26 15:39:57 +0100

    Grid mouseover effect added

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const grid = document.querySelector(".container");
-const gridSize = document.querySelector(".uInput");
-const go = document.querySelector(".goBtn");
-const reset = document.querySelector(".resetBtn");
+const gridSize = document.querySelector("#uInput");
 
 
 // Set default grid
@@ -10,36 +8,12 @@ function defaultGrid() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add("square");
+        
+        div.addEventListener("mouseenter", function() {
+            div.style.backgroundColor = 'black';
+        })
         grid.appendChild(div);
-    }
-};
-
-// Update grid with new, user input value
-function newGrid() {
-    grid.innerHTML = '';
-    grid.style.setProperty(
-        "grid-template-columns",
-        `repeat(${gridSize.value}, 2fr)`
-    );
-    grid.style.setProperty(
-        "grid-template-rows",
-        `repeat(${gridSize.value}, 2fr)`
-    );
-    for (let i = 0; i < gridSize.value * gridSize.value ; i++) {
-        const div = document.createElement("div");
-        div.classList.add("square");
-        grid.appendChild(div);
-    }
-    console.log(gridSize.value);
+    };
 };
 
 defaultGrid();
-
-// Changes the class of square from square to color on mouseover
-const square = document.querySelector("div");
-square.addEventListener("mouseover", function(event) {
-    event.target.classList.replace("square", "color");
-});
-
-// Add event listener to grid size input
-gridSize.addEventListener("change", newGrid);
